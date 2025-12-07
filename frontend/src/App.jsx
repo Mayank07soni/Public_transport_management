68a88ef05bc4abec67c4b677c29fbb4be187ff92
Mayank Soni <202451104@iiitvadodara.ac.in>
2025-12-07 23:00:41 +0530

    trainCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage.jsx";
-
+import TrainPage from "./components/TrainPage.jsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/MainPage" element={<MainPage />} />
+        <Route path="/TrainDetailsPage" element={<TrainPage />} />
       </Routes>
     </BrowserRouter>
   );
